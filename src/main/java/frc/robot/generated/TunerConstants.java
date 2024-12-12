5f2c164d2b616d4459bab945444bb0befa959cb8
Robotics4230 <robotics@marshallschool.org>
2024-12-12 15:31:58 -0600

    our constants

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -30,10 +30,9 @@ public class TunerConstants {
           .withKI(0)
           .withKD(0.5)
           .withKS(0.2)
-          .withKV(1.59)
+          .withKV(2.33)
           .withKA(0)
           .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
-
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
@@ -76,14 +75,14 @@ public class TunerConstants {
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.733);
+  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.41);
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
-  private static final double kCoupleRatio = 3.5;
+  private static final double kCoupleRatio = 3.125;
 
-  private static final double kDriveGearRatio = 4.8;
-  private static final double kSteerGearRatio = 150.0 / 7.0;
+  private static final double kDriveGearRatio = 5.902777777777778;
+  private static final double kSteerGearRatio = 18.75;
   private static final Distance kWheelRadius = Inches.of(2);
 
   private static final boolean kInvertLeftSide = false;
@@ -126,48 +125,48 @@ public class TunerConstants {
           .withDriveFrictionVoltage(kDriveFrictionVoltage);
 
   // Front Left
-  private static final int kFrontLeftDriveMotorId = 1;
-  private static final int kFrontLeftSteerMotorId = 2;
-  private static final int kFrontLeftEncoderId = 9;
-  private static final Angle kFrontLeftEncoderOffset = Rotations.of(-0.360840);
+  private static final int kFrontLeftDriveMotorId = 2;
+  private static final int kFrontLeftSteerMotorId = 1;
+  private static final int kFrontLeftEncoderId = 3;
+  private static final Angle kFrontLeftEncoderOffset = Rotations.of(0.322998046875);
   private static final boolean kFrontLeftSteerMotorInverted = true;
   private static final boolean kFrontLeftCANcoderInverted = false;
 
-  private static final Distance kFrontLeftXPos = Inches.of(11.375);
-  private static final Distance kFrontLeftYPos = Inches.of(11.375);
+  private static final Distance kFrontLeftXPos = Inches.of(7.375);
+  private static final Distance kFrontLeftYPos = Inches.of(7.375);
 
   // Front Right
-  private static final int kFrontRightDriveMotorId = 3;
+  private static final int kFrontRightDriveMotorId = 5;
   private static final int kFrontRightSteerMotorId = 4;
-  private static final int kFrontRightEncoderId = 10;
-  private static final Angle kFrontRightEncoderOffset = Rotations.of(0.3135);
+  private static final int kFrontRightEncoderId = 6;
+  private static final Angle kFrontRightEncoderOffset = Rotations.of(0.49072265625);
   private static final boolean kFrontRightSteerMotorInverted = true;
   private static final boolean kFrontRightCANcoderInverted = false;
 
-  private static final Distance kFrontRightXPos = Inches.of(11.375);
-  private static final Distance kFrontRightYPos = Inches.of(-11.375);
+  private static final Distance kFrontRightXPos = Inches.of(7.375);
+  private static final Distance kFrontRightYPos = Inches.of(-7.375);
 
   // Back Left
-  private static final int kBackLeftDriveMotorId = 5;
-  private static final int kBackLeftSteerMotorId = 6;
-  private static final int kBackLeftEncoderId = 11;
-  private static final Angle kBackLeftEncoderOffset = Rotations.of(0.488770);
+  private static final int kBackLeftDriveMotorId = 11;
+  private static final int kBackLeftSteerMotorId = 10;
+  private static final int kBackLeftEncoderId = 12;
+  private static final Angle kBackLeftEncoderOffset = Rotations.of(-0.143798828125);
   private static final boolean kBackLeftSteerMotorInverted = true;
   private static final boolean kBackLeftCANcoderInverted = false;
 
-  private static final Distance kBackLeftXPos = Inches.of(-11.375);
-  private static final Distance kBackLeftYPos = Inches.of(11.375);
+  private static final Distance kBackLeftXPos = Inches.of(-7.375);
+  private static final Distance kBackLeftYPos = Inches.of(7.375);
 
   // Back Right
-  private static final int kBackRightDriveMotorId = 7;
-  private static final int kBackRightSteerMotorId = 8;
-  private static final int kBackRightEncoderId = 12;
-  private static final Angle kBackRightEncoderOffset = Rotations.of(0.1665);
+  private static final int kBackRightDriveMotorId = 8;
+  private static final int kBackRightSteerMotorId = 7;
+  private static final int kBackRightEncoderId = 9;
+  private static final Angle kBackRightEncoderOffset = Rotations.of(0.236328125);
   private static final boolean kBackRightSteerMotorInverted = true;
   private static final boolean kBackRightCANcoderInverted = false;
 
-  private static final Distance kBackRightXPos = Inches.of(-11.375);
-  private static final Distance kBackRightYPos = Inches.of(-11.375);
+  private static final Distance kBackRightXPos = Inches.of(-7.375);
+  private static final Distance kBackRightYPos = Inches.of(-7.375);
 
   public static final SwerveModuleConstants FrontLeft =
       ConstantCreator.createModuleConstants(
@@ -214,17 +213,6 @@ public class TunerConstants {
           kBackRightSteerMotorInverted,
           kBackRightCANcoderInverted);
 
-  // /**
-  //  * Creates a CommandSwerveDrivetrain instance.
-  //  * This should only be called once in your robot program,.
-  //  */
-  // public static CommandSwerveDrivetrain createDrivetrain() {
-  //     return new CommandSwerveDrivetrain(
-  //         DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight
-  //     );
-  // }
-
-  /** Creates a DriveIOCTRE instance. This should only be called once in your robot program,. */
   public static DriveIOCTRE createDrivetrain() {
     return new DriveIOCTRE(DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight);
   }
